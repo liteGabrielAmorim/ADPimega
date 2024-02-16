97ed5b3fed2b6536f78e980e0a91b0284fb22a0f
Filipe Cavalcanti <filipe.cavalcanti@pitec.co>
2024-02-16 13:23:25 -0300

    Improvements on update.sh

--- install/update.sh
+++ install/update.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-set -x
+set -e
 
 if [ "$EUID" -ne 0 ]
   then echo "This installer must be run as root"
-  exit
+  exit 1
+fi
+
+if [ -z "${PIMEGA_PSS}" ]; then
+    echo "PIMEGA_PSS is not set"
+    exit 1
+else
+    echo "PIMEGA_PSS: $PIMEGA_PSS"
 fi
 
 rm -rf /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
-cp -r ../../epics /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
+cp -r ../ /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
 cd $PIMEGA_PSS/api
 bash build.sh -e
 cd /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
