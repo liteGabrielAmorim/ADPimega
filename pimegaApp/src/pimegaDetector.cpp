64c54bee33c3321a2b6bae36da9a84c0a9e102ee
Álvaro Costa <alvaro.costa@lumentum.com>
2024-08-13 11:57:08 -0300

    Set camera num capture when numexp changes
    `SetAcqParamCameraNumCapture()` sets the `noOfAcquisitions` backend parameter
    and should be called when the number of exposures is changed, otherwise the
    backend will only write a single frame.

--- pimegaApp/src/pimegaDetector.cpp
+++ pimegaApp/src/pimegaDetector.cpp
@@ -318,12 +318,12 @@ void pimegaDetector::finishAcq(int trigger, int *acquire, int *acquireStatus,
                    __func__);
       UPDATEIOCSTATUS("Acquisition finished");
       *recievedBackendCountOffset += numExposuresVar;
-      break;
 
       *acquire = 0;
       setIntegerParam(ADAcquire, 0);
       *acquireStatus = 0;
       setIntegerParam(ADStatus, ADStatusIdle);
+      break;
   }
 }
 
@@ -1833,6 +1833,9 @@ asynStatus pimegaDetector::setDefaults(void) {
                GetModuleBiasVoltage(pimega, PIMEGA_THREAD_MAIN));
 
   setParameter(PimegaLogFile, this->log_file_path);
+
+  SetAcqParamCameraNumCapture(pimega, 1);
+
   callParamCallbacks();
   return asynSuccess;
 }
@@ -2333,6 +2336,7 @@ asynStatus pimegaDetector::numExposures(unsigned number) {
     return asynError;
   }
   setParameter(ADNumExposures, (int)number);
+  SetAcqParamCameraNumCapture(pimega, number);
   return asynSuccess;
 }
 
